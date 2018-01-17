8aba7c1192931ce1e2a52ec219ed2e619a787de8
Kayvon Tehranian <ktehranian@gmail.com>
2018-01-17 12:27:49 -0800

    Introduce basic functionality to contract

--- contracts/Collect.sol
+++ contracts/Collect.sol
@@ -2,4 +2,15 @@ pragma solidity ^0.4.2;
 
 contract Collect {
 
+   mapping (address => uint256) public balances;
+
+   function contribute(address _contributor, uint256 _amount) public returns (bool) {
+     balances[_contributor] += _amount;
+     return true;
+   }
+
+   function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
 }
