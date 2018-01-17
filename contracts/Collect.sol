197b5b5e58a4e778f08a842b75b5f318cfbebcab
Kayvon Tehranian <ktehranian@gmail.com>
2018-01-17 13:35:03 -0800

    Rename balances -> contributions

--- contracts/Collect.sol
+++ contracts/Collect.sol
@@ -2,15 +2,15 @@ pragma solidity ^0.4.2;
 
 contract Project {
 
-   mapping (address => uint256) public balances;
+   mapping (address => uint256) public contributions;
 
    function contribute(address _contributor, uint256 _amount) public returns (bool) {
-     balances[_contributor] += _amount;
+     contributions[_contributor] += _amount;
      return true;
    }
 
-   function balanceOf(address _owner) public view returns (uint256 balance) {
-        return balances[_owner];
+   function balanceOf(address _contributor) public view returns (uint256 balance) {
+        return contributions[_contributor];
     }
 
 }
