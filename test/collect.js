067be24a03b3f3e80100c6000aa5c626f81b06b5
Kayvon Tehranian <ktehranian@gmail.com>
2018-01-17 12:59:14 -0800

    Add add'l test for storing a balance of 100

--- test/collect.js
+++ test/collect.js
@@ -1,14 +1,27 @@
-// Specifically request an abstraction for MetaCoin
 var Collect = artifacts.require("Collect");
 
 contract('Collect', function(accounts) {
 
   it("should set initial balances to 0", function() {
    return Collect.deployed().then(function(instance) {
-     return instance.balanceOf.call(accounts[0]);
+     return instance.balanceOf(accounts[0]);
    }).then(function(balance) {
-     assert.equal(balance.valueOf(), 0, "No money should have been collected.");
+     assert.equal(balance, 0, "No money should have been collected.");
    });
  });
+
+ it("should store a balance of 100", function() {
+   var amount = 100;
+   var collect;
+
+  return Collect.deployed().then(function(instance) {
+    collect = instance;
+    return collect.contribute(accounts[0], amount);
+  }).then(function(success) {
+    return collect.balanceOf(accounts[0]);
+  }).then(function(balance) {
+    assert.equal(balance, amount, "A balance of 100 should be stored.");
+  });
+});
 
 });
