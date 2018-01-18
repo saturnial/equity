4dc5af25e1f4bd428a4c8836e2f8c7074f196c9a
Kayvon Tehranian <ktehranian@gmail.com>
2018-01-17 16:23:12 -0800

    Add constructor

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -2,8 +2,16 @@ pragma solidity ^0.4.2;
 
 contract Project {
 
+  address owner;
+  uint fundraisingTarget;
+
    mapping (address => uint) public contributions;
    uint public totalAmountRaised;
+
+   function Project(address _projectOwner, uint _target) public {
+     owner = _projectOwner;
+     fundraisingTarget = _target;
+   }
 
    function contribute(address _contributor, uint _amount) public returns (bool) {
      contributions[_contributor] += _amount;
