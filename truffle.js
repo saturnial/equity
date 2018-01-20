446c0ccd1155c4090fc2e57d027729a55347961a
Kayvon Tehranian <ktehranian@gmail.com>
2018-01-20 12:17:02 -0800

    Add ports for ganache-cli and gui

--- truffle.js
+++ truffle.js
@@ -1,11 +1,12 @@
 module.exports = {
   networks: {
-development: {
-host: "localhost",
-port: 8545,
-network_id: "*" // Match any network id
-}
-}
+    development: {
+      host: "localhost",
+      port: 7545, // ganache-gui
+      // port: 8545, // ganache-cli
+      network_id: "*" // Match any network id
+    }
+  }
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
 };
